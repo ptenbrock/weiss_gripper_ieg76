f05533a80753b60ef2625ec1f5c783f470675afe
ipa-pgt <philipp.tenbrock@ipa.fraunhofer.de>
2018-04-16 19:28:42 +0200

    check service arguments and set default force

--- src/driver.py
+++ src/driver.py
@@ -14,13 +14,6 @@ class Driver(object):
 
 		self.driver_logic = DriverLogic(self.serial_port_comm)
 
-		serv_ref = rospy.Service('reference', Trigger, self.handle_reference)
-		serv_ref = rospy.Service('open', Move, self.handle_open)
-		serv_ref = rospy.Service('close', Move, self.handle_close)
-		serv_ref = rospy.Service('grasp', Move, self.handle_grasp)
-		# serv_ref = rospy.Service('ack', Trigger, self.handle_ack)
-		serv_ref = rospy.Service('set_force', SetForce, self.handle_set_force)
-
 		self.states_publisher_thread = StatesPublisher(0.8, self.serial_port_comm)
 		rospy.on_shutdown(self.shutdown_handler)
 
@@ -29,6 +22,12 @@ class Driver(object):
 			rospy.loginfo(reply.message)
 		else:
 			rospy.logerr(reply.message)
+
+	def check_position(self, pos):
+		return 0 <= pos <= 30
+
+	def check_force(self, force):
+		return 0 <= force <= 100
 
 	def handle_reference(self, req):
 		rospy.loginfo("Referencing")
@@ -41,32 +40,51 @@ class Driver(object):
 	def handle_open(self, req):
 		rospy.loginfo("Opening")
 		reply = MoveResponse()
-		self.driver_logic.service_called(transition="do_open", params=req, trigger_response=reply)
-		reply.message = 'Opening ' + reply.message
+		if not self.check_position(req.position):
+			reply.success = False
+			reply.message = 'Opening failed. Position must be 0.0(mm) <= position <= 30.0(mm).'
+		else:
+			self.driver_logic.service_called(transition="do_open", params=req, trigger_response=reply)
+			reply.message = 'Opening ' + reply.message
 		self.log_reply(reply)
 		return reply
 
 	def handle_close(self, req):
 		rospy.loginfo("Closing")
 		reply = MoveResponse()
-		self.driver_logic.service_called(transition="do_close", params=req, trigger_response=reply)
-		reply.message = 'Closing ' + reply.message
+		if not self.check_position(req.position):
+			reply.success = False
+			reply.message = 'Closing failed. Position must be 0.0(mm) <= position <= 30.0(mm).'
+		else:
+			self.driver_logic.service_called(transition="do_close", params=req, trigger_response=reply)
+			reply.message = 'Closing ' + reply.message
 		self.log_reply(reply)
 		return reply
 
 	def handle_grasp(self, req):
 		rospy.loginfo("Grasping")
 		reply = MoveResponse()
-		self.driver_logic.service_called(transition="do_grasp", params=req, trigger_response=reply)
-		reply.message = 'Grasping ' + reply.message
+		if not self.check_position(req.position):
+			reply.success = False
+			reply.message = 'Grasping failed. Position must be 0.0(mm) <= position <= 30.0(mm).'
+		else:
+			self.driver_logic.service_called(transition="do_grasp", params=req, trigger_response=reply)
+			reply.message = 'Grasping ' + reply.message
 		self.log_reply(reply)
 		return reply
 
 	def handle_set_force(self, req):
 		rospy.loginfo("Set force")
 		reply = SetForceResponse()
-		reply.success = self.serial_port_comm.set_force(req.grasping_force)
-		reply.message = 'Set force successful.'
+		if not self.check_force(req.grasping_force):
+			reply.success = False
+			reply.message = 'Force must be 0(%) <= force <= 100(%).'
+		else:
+			reply.success = self.serial_port_comm.set_force(req.grasping_force)
+		if reply.success:
+			reply.message = 'Set force successful.'
+		else:
+			reply.message = 'Set force failed. ' + reply.message
 		self.log_reply(reply)
 		return reply
 
@@ -83,6 +101,20 @@ class Driver(object):
 		self.serial_port_comm.start()
 		self.states_publisher_thread.start()
 		rospy.logdebug("Threads started.")
+
+		grasp_force = rospy.get_param("~grasp_force", 100)
+		rospy.loginfo('Set force to {}%.'.format(grasp_force))
+		while True:
+			if self.serial_port_comm.set_force(grasp_force):
+				break
+		rospy.loginfo('Force set.')
+
+		serv_ref = rospy.Service('~reference', Trigger, self.handle_reference)
+		serv_ref = rospy.Service('~open', Move, self.handle_open)
+		serv_ref = rospy.Service('~close', Move, self.handle_close)
+		serv_ref = rospy.Service('~grasp', Move, self.handle_grasp)
+		# serv_ref = rospy.Service('ack', Trigger, self.handle_ack)
+		serv_ref = rospy.Service('~set_force', SetForce, self.handle_set_force)
 
 		rospy.loginfo("Ready to receive requests.")
 
